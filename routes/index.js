39d31f8bc529e9f20c071c9dd9bf2ee9154cbaa9
Igor Luiz <igor@devstacks.com.br>
2017-12-03 00:53:24 -0200

    Create index route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ import imageRoutes from './image'
 
 const router = express.Router()
 
+/**
+ * Status route
+ */
+router.get('/', (req, res) => res.json({ message: 'Everything operating normally.' }))
+
 imageRoutes(router)
 
 export default router
